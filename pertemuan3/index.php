38cc8242c56068ca0bf890fa9fba8dcc32a175bf
kakafikri <kakafikri56@gmail.com>
2021-02-22 22:46:02 +0700

    menyelesaikan video pertemuan 3

---
+++ pertemuan3/index.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Fikri Ridwan Faturohman
+203040001
+https://github.com/kakafikri/pw2021_203040001
+Pertemuan 3 - 22 Februari 2021
+Pengulangan
+*/
+?>
+
+<?php
+// Pengulangan
+// for
+// while
+// do.. while
+// foreach : pengulangan khusus array
+
+// for( $i = 0; $i < 5; $i++ ) {
+//     echo "Hello World! <br>";
+// }
+
+// $i = 0;
+// while( $i < 5 ) {
+//     echo "Hello World! <br>";
+// $i++;
+// }
+
+// $i = 0;
+// do {
+//     echo "Hello World! <br>";
+// $i++;
+// } while( $i < 5 );
+?>
---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Fikri Ridwan Faturohman
+203040001
+https://github.com/kakafikri/pw2021_203040001
+Pertemuan 3 - 22 Februari 2021
+Pengulangan dan Pengkondisian
+*/
+?>
+
+<?php
+// Pengkondisian / Percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 30;
+if( $x < 20 ) {
+    echo "benar";
+} else if( $x == 20 ) {
+    echo "bingo!";
+} else {
+    echo "salah";
+}
+?>
